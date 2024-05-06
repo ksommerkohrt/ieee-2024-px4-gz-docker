c8fec3822b90b4bcf33d159a9ac54c133d808046
ksommerkohrt <90289409+ksommerkohrt@users.noreply.github.com>
2024-05-06 18:50:58 -0400

    Update get_src.sh
    added a repo w/ a launch file so bridging the lidar messages is easier

--- get_src.sh
+++ get_src.sh
@@ -13,6 +13,6 @@ if [ ! -d ./work/ros2_ws/src ] ; then
     git clone git@github.com:PX4/px4_msgs.git
     git clone git@github.com:Jaeyoung-Lim/px4-offboard.git
     git clone git@github.com:mavlink/mavros.git
-    git clone -b humble git@github.com:gazebosim/ros_gz.git
+    git clone -b humble git@github.com:melodylylin/ros_gz.git
 fi
 
